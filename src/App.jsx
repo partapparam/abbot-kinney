04f006da147abeef8dedacd1a265888d11c82025
partapparam <paramsingh1961@gmail.com>
2024-01-24 12:25:32 -0800

    framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { LetterProvider } from "./providers/letterContext"
 import AdminTable from "./components/adminTable"
 import AdminEditor from "./components/adminEditor"
 import AdminLogin from "./components/adminLogin"
+import { AnimatePresence } from "framer-motion"
 
 const App = () => {
   // location will provide us the current location of the app
@@ -15,15 +16,17 @@ const App = () => {
   const location = useLocation()
   return (
     <LetterProvider>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Letter />} />
-        <Route path="/thank-you" element={<Home />} />
-        <Route path="/login" element={<AdminLogin />} />
-        <Route path="/admin" element={<Admin />}>
-          <Route path="editor/:type/:id" element={<AdminEditor />} />
-          <Route index element={<AdminTable />} />
-        </Route>
-      </Routes>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Letter />} />
+          <Route path="/thank-you" element={<Home />} />
+          <Route path="/login" element={<AdminLogin />} />
+          <Route path="/admin" element={<Admin />}>
+            <Route path="editor/:type/:id" element={<AdminEditor />} />
+            <Route index element={<AdminTable />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
     </LetterProvider>
   )
 }
